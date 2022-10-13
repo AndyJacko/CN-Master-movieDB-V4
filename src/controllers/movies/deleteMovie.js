34f8cc921d32a9bb28d78a714e7096553ead3899
Andy Jacko <wisdom.mcr@gmail.com>
2022-10-13 11:44:41 +0100

    Delete Movies - feature complete...

--- src/controllers/movies/deleteMovie.js
+++ src/controllers/movies/deleteMovie.js
@@ -1,5 +1,40 @@
 const Movie = require("../../models/movie");
 
-exports.deleteMovie = async () => {
-  console.log("Delete Movie");
+const displayDeletedMovie = (movie) => {
+  console.log(`\n\nMOVIE DELETED\n--------------------\n`);
+
+  console.log(
+    `ID: ${movie._id}\nTitle: ${movie.title.toUpperCase()}\nActor: ${
+      movie.actor
+    }\nDirector: ${movie.director}\nGenre: ${movie.genre}\nReleased: ${
+      movie.released
+    }\nRating: ${movie.rating}\n`
+  );
+};
+
+exports.deleteMovie = async (id) => {
+  let error;
+  let deletedMovie = false;
+
+  if (id && id !== true && id.length === 24) {
+    try {
+      const selectedMovie = await Movie.find({ _id: id });
+      const delMovie = await Movie.deleteOne({ _id: id });
+
+      if (delMovie.deletedCount === 1) {
+        displayDeletedMovie(selectedMovie[0]);
+        deletedMovie = true;
+      } else {
+        error = `No Movie Found With ID: ${id}`;
+      }
+    } catch (err) {
+      error = err;
+    }
+  } else {
+    error = `Unknown ID Format`;
+  }
+
+  if (!deletedMovie) {
+    console.log(`\n\nError: ${error}`);
+  }
 };
